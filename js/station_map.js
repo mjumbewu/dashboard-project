0efeb750b8af0594717832f3a54c2cef4d73eeac
Mjumbe Poe <mjumbewu@gmail.com>
2024-10-17 12:04:16 -0400

    Add a citation for the pulsating circle animation

--- js/station_map.js
+++ js/station_map.js
@@ -14,6 +14,8 @@ function initMap(el, events) {
   const dataLayer = L.layerGroup();
   dataLayer.addTo(map);
 
+  // Create an animated icon for the geolocation marker.
+  // Pulsating circle adapted from https://codepen.io/shaneparsons/pen/MpgEma
   const geolocationIconHtml = `
     <svg width="20" height="20" viewbox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
       <circle class="pulsing-circle" cx="20" cy="20" r="10" fill="none">
