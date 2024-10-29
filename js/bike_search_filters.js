c684c380ad7e0f32938ec5d8fb4c759c48e04450
Mjumbe Poe <mjumbewu@gmail.com>
2024-10-29 10:36:29 -0400

    Store and retrieve the user's bike preferences from local storage

--- js/bike_search_filters.js
+++ js/bike_search_filters.js
@@ -2,6 +2,21 @@ function initBikeSearchFilters(el, events) {
   const bikeTypeSelect = el.querySelector('[name="bike-type"]');
   const batterySlider = el.querySelector('[name="min-battery-level"]');
 
+  function syncFromLocalStorage() {
+    const bikeType = localStorage.getItem('bikeType');
+    if (bikeType) {
+      bikeTypeSelect.value = bikeType;
+    }
+
+    const minBattery = localStorage.getItem('minBattery');
+    if (minBattery) {
+      batterySlider.value = parseInt(minBattery);
+    }
+
+    emitBikeFilterChange();
+  }
+  syncFromLocalStorage();
+
   function isElectricBikesAllowed() {
     return bikeTypeSelect.value === 'electric' || bikeTypeSelect.value === 'all';
   }
@@ -29,16 +44,20 @@ function initBikeSearchFilters(el, events) {
     batterySlider.disabled = !force;
   }
 
-  bikeTypeSelect.addEventListener('change', (e) => {
-    toggleBatterySlider();
-
+  function emitBikeFilterChange() {
     const evt = new CustomEvent('bikefilterchange', { detail: bikeFilterDetails() });
     events.dispatchEvent(evt);
+  }
+
+  bikeTypeSelect.addEventListener('change', (e) => {
+    localStorage.setItem('bikeType', bikeTypeSelect.value);
+    toggleBatterySlider();
+    emitBikeFilterChange();
   });
 
   batterySlider.addEventListener('input', (e) => {
-    const evt = new CustomEvent('bikefilterchange', { detail: bikeFilterDetails() });
-    events.dispatchEvent(evt);
+    localStorage.setItem('minBattery', batterySlider.value);
+    emitBikeFilterChange();
   });
 
   toggleBatterySlider();
